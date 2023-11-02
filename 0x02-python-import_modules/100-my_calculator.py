8e9060c1fd7ce8476f3505e1e769f56bcb3063d0
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-02 07:46:09 -0700

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 
 if __name__ == '__main__':
-    if len(argv) == 1:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     operator = argv[2]
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == '__main__':
     argc = len(argv) - 1
     if not argc:
         print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:".format(argc))
+        print("{}: {}".format(1, argv[1]))
     else:
         print("{} arguments:".format(argc))
         for i in range(1, argc+1):
