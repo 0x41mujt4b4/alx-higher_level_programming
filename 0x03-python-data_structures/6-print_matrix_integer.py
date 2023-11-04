37245805dcd205d0062fb274745bff387511472b
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-04 12:11:40 -0700

    fix requirement error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     if not any(matrix):
         print()
-    l = len(matrix)
+    outer_len = len(matrix)
     i = j = 0
-    for i in range(l):
-        ll = len(matrix[i])
-        for j in range(ll):
-            if j != ll-1:
+    for i in range(outer_len):
+        inner_len = len(matrix[i])
+        for j in range(inner_len):
+            if j != inner_len - 1:
                 print("{:d}".format(matrix[i][j]), end=" ")
             else:
                 print("{:d}".format(matrix[i][j]))
