128371ef8fa1a41c162824973c7e40b98453dd11
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-04 04:02:51 -0700

    add 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = a2 = b1 = b2 = 0
+    if len(tuple_a) < 2:
+        if len(tuple_1) == 0:
+            a1 = b1 = 0
+        else:
+            a1 = tuple_a[0]
+            b1 = 0
+    else:
+        a1, b1 = tuple_a
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            a2 = b2 = 0
+        else:
+            a2 = tuple_b[0]
+            b2 = 0
+    else:
+        a2, b2 = tuple_b
+    return a1+a2, b1+b2
