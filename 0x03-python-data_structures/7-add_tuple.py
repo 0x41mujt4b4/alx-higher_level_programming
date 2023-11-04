f353fa030a26fb5b07dc032b80e7c6757061abdd
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-04 12:25:01 -0700

    update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a1 = a2 = b1 = b2 = 0
     if len(tuple_a) < 2:
-        if len(tuple_1) == 0:
+        if len(tuple_a) == 0:
             a1 = b1 = 0
         else:
             a1 = tuple_a[0]
