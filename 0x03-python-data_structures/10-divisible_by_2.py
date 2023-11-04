7cd0717be99b0423e761c9aba4e95352be986484
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-04 12:04:13 -0700

    make the files executable

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,3 @@
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     return list(map(lambda n: not n%2, my_list))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     l = len(my_list) - 1
     for i in range(l, -1, -1):
