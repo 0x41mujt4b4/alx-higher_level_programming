4f4bbcd7ecc2d414e528dbcd591fc8d48475fe4a
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-08 11:46:52 -0800

    update 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary.keys():
+    keys = a_dictionary.keys()
+    new_d = a_dictionary.copy()
+    for key in keys:
         if a_dictionary[key] == value:
-            del a_dictionary[key]
-            return a_dictionary
+            del new_d[key]
+    return new_d
