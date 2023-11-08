fd8c38131ce830c8c090b49380720f4495b592e5
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-08 12:06:31 -0800

    update 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def romat_to_int(roman_string):
+def roman_to_int(roman_string):
     roman = {'M': 1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
     result = 0
-    lens = len(s)
+    lens = len(roman_string)
     for i in range(lens):
-        if i+1 < lens and roman[s[i]] < roman[s[i+1]]:
-            result -= roman[s[i]]
+        if i+1 < lens and roman[roman_string[i]] < roman[roman_string[i+1]]:
+            result -= roman[roman_string[i]]
         else:
-            result += roman[s[i]]
+            result += roman[roman_string[i]]
     return result
