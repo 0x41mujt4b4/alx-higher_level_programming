6dcf1a5a958746a47911b1ba69b4cfdca341db05
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-08 12:14:23 -0800

    fix pycodestyle error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    roman = {'M': 1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     result = 0
     lens = len(roman_string)
     for i in range(lens):
