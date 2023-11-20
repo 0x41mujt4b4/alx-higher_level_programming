09229ef87ad66816c771f6ce10179acff0441107
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-20 07:12:43 -0800

    update 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             i += 1
     finally:
-        print()
+        print("{}".format(""))
         return i
