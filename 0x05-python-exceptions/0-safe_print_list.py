4bc1b3913db3dccdd73dadbc9f0333bf11ce86ce
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-20 07:52:31 -0800

    update 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    if x <= 0:
-        return 0
     counter = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             counter += 1
-        except Exception:
+        except IndexError:
             break
     print()
     return counter
+
