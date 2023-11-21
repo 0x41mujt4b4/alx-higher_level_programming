3fdec4a2b8e244e56dbd589eb516e642f011d49e
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-21 06:03:01 -0800

    update 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         """A method that return square object area"""
-        return self.size * self.size
+        return self.__size * self.__size
