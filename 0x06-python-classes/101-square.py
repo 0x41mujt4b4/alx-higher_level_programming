c0bff520b13eca34b271a93f916ef1dfb29a35e5
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-21 23:42:26 -0800

    update 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -63,7 +63,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-    def my_print(self):
+    def __str__(self):
         """prints in stdout the square with the character #
         """
 
