44997812d661b2cf2078c7b83897f79e3694e63e
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-27 12:16:41 -0800

    update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,9 +2,10 @@
 
 def can_place(board, row, col):
     for i in range(col):
-        if board[i] == row or \
-            board[i] - i == row - col or \
-                board[i] + i == row + col:
+        queen_can_attack = [board[i] == row,
+            board[i] - i == row - col,
+            board[i] + i == row + col]
+        if any(queen_can_attack):
             return False
     return True
 
@@ -13,17 +14,15 @@ def place_queens(board, col, n):
     if col == n:
         result.append(board[:])
         return
-
-        for row in range(n):
-            if can_place(board, row, col):
-                board[col] = row
-                place_queens(board, col + 1, n)
-                board[col] = -1
+    for row in range(n):
+        if can_place(board, row, col):
+            board[col] = row
+            place_queens(board, col + 1, n)
+            board[col] = -1
 
 
 if __name__ == '__main__':
     import sys
-
     n = int(sys.argv[1])
     result = []
     board = [-1] * n
