00016b762ed1de4e620fa068ac873f92555544a4
0x41mujt4b4 <almujtaba007@gmail.com>
2023-12-04 10:01:40 -0800

    add 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define module is same class"""
+
+
+def is_same_class(obj, a_class):
+    """function that returns true if obj instance of a_class"""
+    return isinstance(obj, a_class)
