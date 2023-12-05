6684a3e2f6f5f1450db75f4c2acbf3b6eaad2d19
0x41mujt4b4 <almujtaba007@gmail.com>
2023-12-05 05:40:24 -0800

    add 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that contain one function"""
+import json
+
+
+def to_json_string(my_obj):
+    """function to convert object to json string"""
+    return json.dumps(my_obj)
