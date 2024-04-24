f5aa22723128829e9e9f9103b80de89f61ec2352
root <almujtaba.abdu@gmail.com>
2024-04-24 15:29:48 -0700

    update 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 // function that returns the number of occurrences in a list
+const { dict } = require('./101-data.js');
 
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-  return (e * index);
-});
-console.log(mappedList);
+const newDict = {};
+for (const key in dict) {
+  const value = dict[key];
+  if (newDict[value]) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [key];
+  }
+}
+
+console.log(newDict);
