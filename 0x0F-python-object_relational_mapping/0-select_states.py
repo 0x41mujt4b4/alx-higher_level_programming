505762ecdff5dc992d0a48522600af2a9d5a9aa8
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-28 12:24:31 -0700

    update 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 user = argv[1]
 passwd = argv[2]
 db = argv[3]
-conn = MySQLdb.CONNECT(host="localhost", port=3306,
+conn = MySQLdb.connect(host="localhost", port=3306,
                             user=user, passwd=passwd, db=db, charset="utf8")
 
 
