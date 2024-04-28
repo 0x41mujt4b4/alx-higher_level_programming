bbd9bf90acb8e4b88fdbec7e9732d7b02717802b
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-28 13:31:57 -0700

    update 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                                 user=user, passwd=passwd,
                                 db=db, charset="utf8")
     cur = conn.cursor()
-    cur.excute("SELECT * FROM `states` ORDER BY id ASC")
+    cur.execute("SELECT * FROM `states` ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
