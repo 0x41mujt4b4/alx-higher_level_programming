a67944fba85050a905f9f0732a9aa3cb19146bf2
0x41mujt4b4 <almujtaba007@gmail.com>
2024-05-02 07:16:04 -0700

    update model_state & add venv

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,12 +6,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """state table class"""
     __tablename__ = 'states'
-    id = Column("id", Integer, primary_key=True, nullable=False)
+    id = Column("id", Integer(11), primary_key=True, nullable=False)
     name = Column("name", String(128), nullable=False)
-    
+
     def __init__(self, id, name):
         self.id = id
         self.name = name
